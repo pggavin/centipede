ae8d7c06a954bbd2c2e64820586808befb47d34e
VFS\pg24gavin <pg24gavin@vfs.com>
2023-01-31 10:16:53 -0800

    initial version

---
+++ index.js
@@ -0,0 +1,14 @@
+// Copyright (C) 2022 Scott Henshaw
+
+// import App from './scripts/App.js'
+
+document.addEventListener('DOMContentLoaded', event => {
+    // Our code goes here....
+    // const app = new App()
+    const el = document.querySelector("#actual-img");
+    const msg = "My special code";
+    el.innerHTML = `<div id="stuff">${msg}</div>`;
+    el.style.backgroundColor = "rgba(107,43,55,1)"
+
+    // Build the table
+})
